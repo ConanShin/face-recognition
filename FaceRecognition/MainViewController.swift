c4dfaef8b3d67888117b4caf35f5d439fea13d32
ConanShin <conan.shin@sk.com>
2020-02-27 15:01:08 +0900

    Initial Commit

---
+++ FaceRecognition/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  FaceRecognition
+//
+//  Created by 신철민님/Smart Office TF on 2020/02/27.
+//  Copyright © 2020 Conan Shin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
